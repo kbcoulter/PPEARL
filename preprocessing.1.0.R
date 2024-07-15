a71d000dbd8dc7079f3967f4456702e7e00dfbec
VollbreR <Vollbrer@bc.edu>
2024-07-15 13:07:21 -0600

    Update preprocessing.1.0.R

--- preprocessing.1.0.R
+++ preprocessing.1.0.R
@@ -5,6 +5,7 @@ filepath = "~/Desktop/CoSIBS/NPLCM/Carpe Copy 7:11.csv"
 threshold = 0.9
 fill_columns = c("virus_1", "virus_2",	"virus_3",	"bacteria_1")
 
+meep <- read_csv("C:\\Users\\robvo\\OneDrive\\Desktop\\CoSibs_Project\\Copy of MEEPData.csv") ##Kaden Change this##
 carpe = read.csv(filepath)
 rows = nrow(carpe)
 
@@ -27,3 +28,16 @@ drop = names(missing[missing > thresh])
 carpe_clean = carpe[, !(names(carpe) %in% drop)]
 #ncol(carpe_clean)
 
+#Differentiating Study ID's
+#Unique Study ID's for carpe by adding "c" in every study ID value
+stringsAsFactors = FALSE 
+carpe_clean$study_id <- as.character(carpe_clean$study_id)
+carpe_clean$study_id <- sapply(carpe_clean$study_id, function(x) paste0("c", x))
+#head(carpe_clean)
+
+#Unique Study ID's for meep by adding "m" in every study ID value
+#### MEEP NOT CLEANED BELOW###
+stringsAsFactors = FALSE
+meep$study_id <- as.character(meep$study_id)
+meep$study_id <- sapply(meep$study_id, function(x) paste0("m", x))
+#head(meep)
