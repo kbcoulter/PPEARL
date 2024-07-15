50d7ec39f8feaa983930da6692b8dc49ebced89d
Kaden Coulter <156264224+kbcoulter@users.noreply.github.com>
2024-07-15 12:33:17 -0700

    Add files via upload

---
+++ preprocessing1.1.R
@@ -0,0 +1,60 @@
+#Data Preprocessing
+
+# Imports
+library(dplyr)
+library(tidyverse)
+library(readxl)
+
+# Set Carpe File Path, Threshold, columns where 0 fill are needed (NA -> 0), and columns to omit
+c_filepath = "copy of CarpeData.csv"
+m_filepath = "Copy of MEEPData.xlsx"
+threshold = 0.9
+
+fill_columns = c("virus_1", "virus_2",	"virus_3",	"bacteria_1")
+columns_to_cut = c('transed', 'sx19_oth1', 'sxdays19_1', 'sx19_oth2', 'sx19_oth3', 'fuwhere1', 'fuwhere_oth1', 
+                   'fuabxchgoth1', 'sx19_oth4', 'indeathdat', 'sx19_oth5', 'immun_ageno', 'edurinmeth', 'edcxrint_oth', 'immun_age', 
+                   'immun_ageu', 'abx_name_oth', 'concern_about', 'zipcode', 'edurinvol', 'edurindat', 'edurintm', 'edurinno', 'edurinno_oth', 
+                   'edsampfrig_dat2', 'edsampfrig_tm2', 'indx13dat', 'indeathdat', 'inurinvol', 'inurindat', 'inurintm', 'inurinno_oth', 
+                   'insampfrig_urin_dat', 'insampfrig_urin_tm', 'readdat1', 'readtim1', 'rexfericuwhy_oth1', 'redx13dat2', 'rexfericudat2',
+                   'rexfericuwhy_oth2', 'readdat3', 'readtim3', 'readdiag13', 'redx13dat3', 'rexfericuwhy_oth3', 'fusx_oth', 'fuabxnottk_oth', 
+                   'fuqol', 'fuwhere_oth1', 'fuabxchgoth1', 'bnbionbr', 'race___1', 'race___', 'race___3', 'race___4', 'race___5', 'race___6', 
+                   'race___99', 'race_oth', 'ethnicity', 'sch_mother')
+
+meep = read_excel(m_filepath)
+carpe = read.csv(c_filepath)
+rows = nrow(carpe)
+
+# Drop specified columns from the Dataframe
+carpe_inter <- carpe[, !(names(carpe) %in% columns_to_cut)]
+
+# Replace NA with 0 in the specified columns
+carpe[fill_columns] = lapply(carpe[fill_columns], function(x) {
+  x[is.na(x)] = 0
+  return(x)
+})
+
+# Replace all values of 99 with NA in the data frame (99 = Unknown)
+carpe[] = lapply(carpe, function(x) {
+  x[x == 99] = NA
+  return(x)
+})
+
+# Dropping Data Below Specified Missing Threshold ("Drop" Holds All Dropped Columns)
+thresh = threshold * nrow(carpe)
+missing = sapply(carpe, function(x) sum(is.na(x)))
+drop = names(missing[missing > thresh])
+carpe_clean = carpe[, !(names(carpe) %in% drop)]
+#ncol(carpe_clean)
+
+# Differentiating Study ID's
+# carpe -> "c"
+stringsAsFactors = FALSE 
+carpe_clean$study_id <- as.character(carpe_clean$study_id)
+carpe_clean$study_id <- sapply(carpe_clean$study_id, function(x) paste0("c", x))
+
+# meep -> "m" 
+#### MEEP NOT CLEANED BELOW###
+stringsAsFactors = FALSE
+meep$study_id <- as.character(meep$study_id)
+meep$study_id <- sapply(meep$study_id, function(x) paste0("m", x))
+
