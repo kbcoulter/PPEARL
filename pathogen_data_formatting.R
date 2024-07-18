9dc693845a606272d141575b696537e195d0a541
Jacob Sussman <99224973+jsussman22@users.noreply.github.com>
2024-07-17 21:24:14 -0600

    format pathogen test data for model

---
+++ pathogen_data_formatting.R
@@ -0,0 +1,116 @@
+
+
+carp_pathogens <- carpe_data |>
+  select(virus_1, virus_2, virus_3, viraltestrun, bacteria_1, bacterialtestrun) |>
+  mutate(v1 = case_when(virus_1 == 1 |
+                          virus_2 == 1 | virus_3 == 1 ~ 1, .default = 0)) |> 
+  mutate(v2 = case_when(virus_1 == 2 |
+                          virus_2 == 2 | virus_3 == 2 ~ 1, .default = 0)) |> 
+  mutate(v3 = case_when(virus_1 == 3 |
+                          virus_2 == 3 | virus_3 == 3 ~ 1, .default = 0)) |> 
+  mutate(v4 = case_when(virus_1 == 4 |
+                          virus_2 == 4 | virus_3 == 4 ~ 1, .default = 0)) |>
+  mutate(v5 = case_when(virus_1 == 5 |
+                          virus_2 == 5 | virus_3 == 5 ~ 1, .default = 0)) |> 
+  mutate(v6 = case_when(virus_1 == 6 |
+                          virus_2 == 6 | virus_3 == 6 ~ 1, .default = 0)) |> 
+  mutate(v7 = case_when(virus_1 == 7 |
+                          virus_2 == 7 | virus_3 == 7 ~ 1, .default = 0)) |>
+  mutate(v8 = case_when(virus_1 == 8 |
+                          virus_2 == 8 | virus_3 == 8 ~ 1, .default = 0)) |> 
+  mutate(v9 = case_when(virus_1 == 9 |
+                          virus_2 == 9 | virus_3 == 9 ~ 1, .default = 0)) |> 
+  mutate(v10 = case_when(virus_1 == 10 |
+                          virus_2 == 10 | virus_3 == 10 ~ 1, .default = 0)) |> 
+  mutate(v11 = case_when(virus_1 == 11 |
+                          virus_2 == 11 | virus_3 == 11 ~ 1, .default = 0)) |> 
+  mutate(v12 = case_when(virus_1 == 12 |
+                           virus_2 == 12 | virus_3 == 12 ~ 1, .default = 0)) |>
+  mutate(v1 = case_when(is.na(viraltestrun) ~ NA, .default = v1) ) |> 
+  mutate(v2 = case_when(is.na(viraltestrun) ~ NA, .default = v2) ) |> 
+  mutate(v3 = case_when(is.na(viraltestrun) ~ NA, .default = v3) ) |> 
+  mutate(v4 = case_when(is.na(viraltestrun) ~ NA, .default = v4) ) |> 
+  mutate(v5 = case_when(is.na(viraltestrun) ~ NA, .default = v5) ) |> 
+  mutate(v6 = case_when(is.na(viraltestrun) ~ NA, .default = v6) ) |> 
+  mutate(v7 = case_when(is.na(viraltestrun) ~ NA, .default = v7) ) |> 
+  mutate(v8 = case_when(is.na(viraltestrun) ~ NA, .default = v8) ) |> 
+  mutate(v9 = case_when(is.na(viraltestrun) ~ NA, .default = v9) ) |> 
+  mutate(v10 = case_when(is.na(viraltestrun) ~ NA, .default = v10) ) |> 
+  mutate(v11 = case_when(is.na(viraltestrun) ~ NA, .default = v11) ) |> 
+  mutate(v12 = case_when(is.na(viraltestrun) ~ NA, .default = v12) ) |> 
+  mutate(b1 = case_when(bacteria_1 == 1 ~ 1, .default = 0)) |> 
+  mutate(b2 = case_when(bacteria_1 == 2 ~ 1, .default = 0)) |> 
+  mutate(b3 = case_when(bacteria_1 == 3 ~ 1, .default = 0)) |> 
+  mutate(b4 = case_when(bacteria_1 == 4 ~ 1, .default = 0)) |> 
+  mutate(b5 = case_when(bacteria_1 == 5 ~ 1, .default = 0)) |> 
+  mutate(b6 = case_when(bacteria_1 == 6 ~ 1, .default = 0)) |> 
+  mutate(b7 = case_when(bacteria_1 == 7 ~ 1, .default = 0)) |> 
+  mutate(b8 = case_when(bacteria_1 == 8 ~ 1, .default = 0)) |> 
+  mutate(b9 = case_when(bacteria_1 == 9 ~ 1, .default = 0)) |> 
+  mutate(b1 = case_when(is.na(bacterialtestrun) ~ NA, .default = b1) ) |> 
+  mutate(b2 = case_when(is.na(bacterialtestrun) ~ NA, .default = b2) ) |> 
+  mutate(b3 = case_when(is.na(bacterialtestrun) ~ NA, .default = b3) ) |> 
+  mutate(b4 = case_when(is.na(bacterialtestrun) ~ NA, .default = b4) ) |> 
+  mutate(b5 = case_when(is.na(bacterialtestrun) ~ NA, .default = b5) ) |> 
+  mutate(b6 = case_when(is.na(bacterialtestrun) ~ NA, .default = b6) ) |> 
+  mutate(b7 = case_when(is.na(bacterialtestrun) ~ NA, .default = b7) ) |> 
+  mutate(b8 = case_when(is.na(bacterialtestrun) ~ NA, .default = b8) ) |> 
+  mutate(b9 = case_when(is.na(bacterialtestrun) ~ NA, .default = b9) ) |> 
+  select(v1:b9) |> 
+  rename(
+    FLUA = v1,
+    FLUB = v2,
+    RSV = v6,
+    Coronavirus = v10,
+    HMPV = v7,
+    HRV_Entero = v8,
+    Adenovirus = v9,
+    PARAFLU1 = v3,
+    PARAFLU2 = v4,
+    PARAFLU3 = v5,
+    PARAFLU4 = v12,
+    Bocavirus = v11,
+    S_PNEU = b1,
+    MycoPCR = b2,
+    CHLAM = b3,
+    LEGI = b4,
+    HAEM = b5,
+    STAPH = b6,
+    S_PYOG = b7,
+    KLEB = b8,
+    PERT = b9
+  ) 
+  
+meep_pathogens <- meep |>
+  select(FLUA:MycoPCR) |>
+  mutate(
+    S_PNEU = NA,
+    CHLAM = NA,
+    LEGI = NA,
+    HAEM = NA,
+    STAPH = NA,
+    S_PYOG = NA,
+    KLEB = NA,
+    PERT = NA
+  ) 
+ 
+
+type <- c(rep(1,dim(carp_pathogens)[1]), rep(0,dim(meep_pathogens)[1]))
+ 
+pathogen_data <- bind_rows(carp_pathogens, meep_pathogens)
+
+MBS <- pathogen_data |> 
+  select(FLUA:PARAFLU4, MycoPCR)
+
+MSS <- pathogen_data |> 
+  select(S_PNEU, CHLAM:PERT)
+
+
+
+
+
+
+
+
+
+
