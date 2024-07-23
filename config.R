12624a2f2d7203f5295389d6aae17ad2e6550d21
Kaden Coulter <156264224+kbcoulter@users.noreply.github.com>
2024-07-23 12:47:52 -0700

    Update config.R

--- config.R
+++ config.R
@@ -6,3 +6,12 @@ library(knitr)
 library(corrplot)
 library(Hmisc)
 library(ggplot2)
+
+
+preprocessing <- "data_preparation/preprocessing.R"
+pathogen_data_formatting <- "data_preparation/pathogen_data_formatting.R"
+piecharts <- "visualizations/piecharts.R"
+
+source(preprocessing)
+source(piecharts)
+source(pathogen_data_formatting)
