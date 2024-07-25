9d18e727253fdc18c8e65efcccbcf69caa129b1b
Kaden Coulter <156264224+kbcoulter@users.noreply.github.com>
2024-07-24 17:24:31 -0700

    Attempted to format simulate_data.R
    needs major revision

--- data_preparation/simulate_data.R
+++ data_preparation/simulate_data.R
@@ -1,5 +1,8 @@
 library(baker)
-##### Data Sim ~ Unchanged
+
+##### Data Sim ~ NOT SURE WORKS CORRECTLY WHEN THERE IS 1 SUBCLASS (PLCM), 
+##### THIS UNDOUBTEDLY DOES NOT WORK
+
 
 # Note: the example will only run 100 Gibbs sampling steps to save computing time.
 # To produce useful posterior inferences, please modify "mcmc_options" as follows
@@ -14,20 +17,29 @@ working_dir <- tempdir() # <-- create a temporary directory.
 #dir.create(randname)
 #working_dir = randname
 
-K.true  <- 5   # no. of latent subclasses in actual simulation. 
+##### SUBCLASSES SET 5 -> 1 #####
+K.true  <- c(1,0)  # no. of latent subclasses in actual simulation. 
 # If eta = c(1,0), K.true is effectively 1.
-J       <- 21   # no. of pathogens.
-N       <- 1436 # no. of cases/controls.
+
+##### PATHOGENS SET 21 -> 4 #####
+J       <- 4   # no. of pathogens.
+
+##### FROM MERGED.CSV #####
+##### CHANGE TO LEN(MERGED.CSV??) not hard code #####
+N       <- 844 # no. of cases/controls.
 
 # case subclass weight (five values):
-subclass_mix_seq <- c(0,0.25,0.5,0.75,1)
+##### SUBCLASSES SET 5 -> 1 #####
+subclass_mix_seq <- c(1)
+#subclass_mix_seq <- c(0,0.25,0.5,0.75,1)
+
 
 
 NREP   <- 100
 MYGRID <- expand.grid(list(rep   = 1:NREP, # data replication.
                            iter  = seq_along(subclass_mix_seq),# mixing weights.
                            k_fit = c(1,2), # model being fitted: 1 for pLCM; >1 for npLCM.
-                           scn   = 3:1)    # index for different truth; see "scn_collection.R".
+                           scn   = 3:1)    # index for different truth; see "scn_collection.R". ??????????
 )
 
 n_seed   <- nrow(unique(MYGRID[,-3]))
@@ -49,6 +61,7 @@ eta      <- c(curr_mix,1-curr_mix)
 seed_start <- 20161215  
 set.seed(seed_start+seed_seq[SEG])
 
+##### CHANGE BELOW?? #####
 if (scn == 3){
   ThetaBS_withNA <- cbind(c(0.95,0.95,0.55,0.95,0.95,0.95,0.95,0.95,0.55,0.95,0.95,0.95,0.95,0.95,0.55,0.95,0.95,0.95,0.95,0.95,0.55),#subclass 1.
                           c(0.95,0.55,0.95,0.55,0.55,0.55,0.95,0.55,0.95,0.55,0.55,0.55,0.95,0.55,0.95,0.55,0.55,0.55,0.95,0.55,0.95))#subclass 2.
@@ -71,15 +84,16 @@ if (scn == 1){
 }
 
 
-
+##### CHANGE BELOW?? #####
 # the following paramter names are set using names in the 'baker' package:
 set_parameter <- list(
-  cause_list      = c(LETTERS[1:J]),
-  etiology        = c(0.5,0.2,0.15,0.05,0.05,0.05,0.5,0.2,0.15,0.05,0.05,0.05,0.5,0.2,0.15,0.05,0.05,0.05,0.5,0.2,0.15),# same length as cause_list.
-  pathogen_BrS    = LETTERS[1:J],
+  cause_list      = c(LETTERS[1:D]),
+  etiology        = c(0.5,0.2,0.15,0.05),# same length as cause_list.
+  # Bronze Standard 4? 
+  pathogen_BrS    = LETTERS[1:D],
   meas_nm         = list(MBS = c("MBS1")), # a single source of Bronze Standard (BrS) data.
   Lambda          = lambda,              #ctrl mix (subclass weights).
-  Eta             = t(replicate(J,eta)), #case mix; # of rows equals length(cause_list).
+  Eta             = t(replicate(D,eta)), #case mix; # of rows equals length(cause_list).
   PsiBS           = PsiBS_withNA,
   ThetaBS         = ThetaBS_withNA,
   Nu      =     N, # control sample size.
