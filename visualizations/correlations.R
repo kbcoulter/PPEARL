e938ffdbc5a3c89dba6546ed88c70917ffe2943b
Kaden Coulter <156264224+kbcoulter@users.noreply.github.com>
2024-07-26 14:26:43 -0700

    Update correlations vis function and file structure

--- visualizations/correlations.R
+++ visualizations/correlations.R
@@ -1,14 +1,6 @@
-##### Correlation Funcitons/Use
-
-##### TODO: Change to import df from preprocessing with selected variables
+##### Correlation
 
 # Functions ------------------------------------------------
-##### Function: vis_corr_matrix -> Create a Visual Correlation Matrix (Only Use with clean data)
-vis_corr_matrix <- function(df) {
-  df = subset(df, select = -c(study_id))
-  M = cor(df)
-  return(corrplot(M, method="circle"))
-}
 
 ##### Function: flat_corr_matrix -> flatten A Correlation Matrix
 flat_corr_matrix <- function(corr, p) {
@@ -23,7 +15,6 @@ flat_corr_matrix <- function(corr, p) {
 
 ##### Function: list_corr_matrix -> return a List of Unique Correlations
 list_corr_matrix <- function(df) {
-  df = subset(df, select = -c(study_id))
   res = rcorr(as.matrix(df))
   
   result = flat_corr_matrix(res$r, res$P)
@@ -36,6 +27,28 @@ list_corr_matrix <- function(df) {
   return(result_unique)
 }
 
+plot_corr_matrix <- function(df) {
+  res = rcorr(as.matrix(df))
+  cor_matrix <- res$r
+  cor_plot = corrplot(cor_matrix, method = "circle", col = colorRampPalette(c("blue", "purple", "red"))(200),
+                      tl.cex = 1,
+                      cl.cex = 0.8,
+                      tl.col = "black")
+  return(cor_plot)
+}
+
 # Use ------------------------------------------------
-##### correlations: correlations from carpe_clean_filtered
-correlations = list_corr_matrix(carpe_clean_filtered)
+##### CHOOSE VARIABLES OF INTEREST
+selected_columns = c('age_yr', 'severity2', 'season')
+
+##### Process (Ignore)
+library(readr)
+merged <- read_csv("merged_data.csv")
+pd <- read_csv("pathogen_data_grouped.csv")
+corr_df = cbind(merged[selected_columns],pd) 
+corr_df <- mutate_all(corr_df, function(x) as.numeric(as.character(x)))
+
+##### Listed and Plotted Correlations
+listed = list_corr_matrix(corr_df)
+plot = plot_corr_matrix(corr_df)
+plot
