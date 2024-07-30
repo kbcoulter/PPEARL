d0ef5464163da6d6a8c55bb7ce157158b5123732
Kaden Coulter <156264224+kbcoulter@users.noreply.github.com>
2024-07-30 14:03:18 -0700

    Update config.R

--- config.R
+++ config.R
@@ -8,13 +8,21 @@ library(corrplot)
 library(Hmisc)
 library(ggplot2)
 
-
 preprocessing <- "data_preparation/preprocessing.R"
 pathogen_data_formatting <- "data_preparation/pathogen_data_formatting.R"
 barcharts <- "visualizations/BarCharts.R"
-covariates <-"visualizations/correlations_covariates.R"
+covariates <- "visualizations/correlations_covariates.R"
+model_options <- "model/model_options.R"
+mcmc_options <- "model/mcmc_options.R"
+run_model <- "model/run_model.R"
+mcmc_analysis <- "model/mcmc_analysis.R"
 
 source(preprocessing)
 source(barcharts)
 source(pathogen_data_formatting)
-source(covariates)
+#source(covariates)
+source(model_options)
+source(mcmc_options)
+source(run_model)
+source(mcmc_analysis)
+
