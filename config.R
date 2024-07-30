1ae756f8779be6fdd4a6ed9f9a856b92a580f781
Kaden Coulter <156264224+kbcoulter@users.noreply.github.com>
2024-07-30 14:27:32 -0700

    Update config.R

--- config.R
+++ config.R
@@ -1,4 +1,4 @@
-##### TODO: Add the rest... XD
+##### library imports
 library(here)
 library(baker)
 library(tidyverse)
@@ -8,6 +8,7 @@ library(corrplot)
 library(Hmisc)
 library(ggplot2)
 
+##### filepaths
 preprocessing <- "data_preparation/preprocessing.R"
 pathogen_data_formatting <- "data_preparation/pathogen_data_formatting.R"
 barcharts <- "visualizations/BarCharts.R"
@@ -17,6 +18,7 @@ mcmc_options <- "model/mcmc_options.R"
 run_model <- "model/run_model.R"
 mcmc_analysis <- "model/mcmc_analysis.R"
 
+##### run files
 source(preprocessing)
 source(barcharts)
 source(pathogen_data_formatting)
